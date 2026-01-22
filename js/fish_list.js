263ea3f2091a77cb038a891309f556e5c3ac2285
rsomatiari-hue <rso.matiari@gmail.com>
2026-01-22 16:44:07 +0500

    Create fish_list.js

---
+++ js/fish_list.js
@@ -0,0 +1,11 @@
+// Fish Master List with Sizes
+const fishList = [
+    { id: "fs1", name_ur: "Gulfam (گلفام - 1kg size)", name_en: "Gulfam 1kg" },
+    { id: "fs2", name_ur: "Gulfam (گلفام - 2kg size)", name_en: "Gulfam 2kg" },
+    { id: "fs3", name_ur: "Gulfam (گلفام - 3kg+ size)", name_en: "Gulfam 3kg Plus" },
+    { id: "fs4", name_ur: "Kurro (کُڑو - Small)", name_en: "Kurro Small" },
+    { id: "fs5", name_ur: "Kurro (کُڑو - Large)", name_en: "Kurro Large" },
+    { id: "fs6", name_ur: "Palla Machli (پلا مچھلی - Medium)", name_en: "Palla Medium" },
+    { id: "fs7", name_ur: "Palla Machli (پلا مچھلی - VIP Large)", name_en: "Palla VIP Large" },
+    { id: "fs8", name_ur: "Rahu (رہو - Fresh)", name_en: "Rahu Fresh" }
+];
