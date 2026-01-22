7860fa1c3d13a5c01c641476d2548fe3e5bd7009
rsomatiari-hue <rso.matiari@gmail.com>
2026-01-22 16:00:18 +0500

    Create rates_database.js

---
+++ is/rates_database.js
@@ -0,0 +1,24 @@
+// Market-wise Rates Database
+const marketRates = {
+    "matiari": {
+        "v1": "65",   // Aloo
+        "v2": "140",  // Pyaz
+        "v3": "90",   // Tamatar
+        "v4": "400",  // Adrak
+        "v5": "350"   // Lehsan
+    },
+    "hyderabad": {
+        "v1": "60",
+        "v2": "150",
+        "v3": "80",
+        "v4": "380",
+        "v5": "330"
+    },
+    "hala": {
+        "v1": "70",
+        "v2": "145",
+        "v3": "95",
+        "v4": "410",
+        "v5": "360"
+    }
+};
