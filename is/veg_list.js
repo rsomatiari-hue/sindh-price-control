ca320bb15110ff6e2b6dad3b7caddebe59c7cded
rsomatiari-hue <rso.matiari@gmail.com>
2026-01-22 15:58:44 +0500

    Create veg_list.js

---
+++ is/veg_list.js
@@ -0,0 +1,19 @@
+// 70+ Vegetables Master List
+const vegetableList = [
+    { id: "v1", name_ur: "Aloo (آلو)", name_en: "Potato" },
+    { id: "v2", name_ur: "Pyaz (پیاز)", name_en: "Onion" },
+    { id: "v3", name_ur: "Tamatar (ٹماٹر)", name_en: "Tomato" },
+    { id: "v4", name_ur: "Adrak (ادرک)", name_en: "Ginger" },
+    { id: "v5", name_ur: "Lehsan (لہسن)", name_en: "Garlic" },
+    { id: "v6", name_ur: "Gobi (گوبھی)", name_en: "Cauliflower" },
+    { id: "v7", name_ur: "Mator (مٹر)", name_en: "Peas" },
+    { id: "v8", name_ur: "Khera (کھیرا)", name_en: "Cucumber" },
+    { id: "v9", name_ur: "Karela (کریلا)", name_en: "Bitter Gourd" },
+    { id: "v10", name_ur: "Baingan (بینگن)", name_en: "Brinjal" },
+    { id: "v11", name_ur: "Palak (پالک)", name_en: "Spinach" },
+    { id: "v12", name_ur: "Bhindi (بھنڈی)", name_en: "Lady Finger" },
+    { id: "v13", name_ur: "Tinda (ٹنڈا)", name_en: "Apple Gourd" },
+    { id: "v14", name_ur: "Kaddu (کدو)", name_en: "Pumpkin" },
+    { id: "v15", name_ur: "Shimla Mirch (شملہ مرچ)", name_en: "Capsicum" }
+    // Khadim sahib, isi tarah aap baki 70 items yahan add kar sakte hain
+];
